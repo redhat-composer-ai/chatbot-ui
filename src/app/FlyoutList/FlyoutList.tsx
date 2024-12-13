9e1e82b3c2cccb0fdb332870ec9c28284899d9a5
Rebecca Alpert <ralpert@redhat.com>
2024-12-13 11:52:50 -0500

    Add file sidebar

--- src/app/FlyoutList/FlyoutList.tsx
+++ src/app/FlyoutList/FlyoutList.tsx
@@ -17,6 +17,9 @@ interface FlyoutListProps {
   hideFlyout: () => void;
   onFooterButtonClick?: () => void;
   title: string;
+  error?: ErrorObject;
+  isLoading?: boolean;
+  onRetry?: () => void;
 }
 export const FlyoutList: React.FunctionComponent<FlyoutListProps> = ({
   typeWordPlural,
@@ -24,19 +27,33 @@ export const FlyoutList: React.FunctionComponent<FlyoutListProps> = ({
   hideFlyout,
   onFooterButtonClick,
   title,
+  error: errorProp,
+  isLoading: isLoadingProp = true,
+  onRetry,
 }: FlyoutListProps) => {
-  const [error, setError] = React.useState<ErrorObject>();
-  const [items, setItems] = React.useState<CannedChatbot[]>([]);
-  const [filteredItems, setFilteredItems] = React.useState<CannedChatbot[]>([]);
-  const [isLoading, setIsLoading] = React.useState(true);
+  const [error, setError] = React.useState<ErrorObject | undefined>(errorProp);
+  const [originalChatbots, setOriginalChatbots] = React.useState<CannedChatbot[]>([]);
+  const [filteredChatbots, setFilteredChatbots] = React.useState<CannedChatbot[]>([]);
+  // you'll need states for files and filtered files
+  const [isLoading, setIsLoading] = React.useState(isLoadingProp);
   const { nextStep, reloadList, setReloadList } = useFlyoutWizard();
   const location = useLocation();
   const navigate = useNavigate();
   const { flyoutMenuSelectedChatbot, updateFlyoutMenuSelectedChatbot, chatbots, setChatbots } = useAppData();
 
+  // used for file case only
+  React.useEffect(() => {
+    setIsLoading(isLoadingProp);
+  }, [isLoadingProp]);
+
+  // used for file case only
+  React.useEffect(() => {
+    setError(errorProp);
+  }, [errorProp]);
+
   const header = (
     <div className="title-with-label">
-      {title} <Label variant="outline">{items.length}</Label>
+      {title} <Label variant="outline">{originalChatbots.length}</Label>
     </div>
   );
 
@@ -71,8 +88,8 @@ export const FlyoutList: React.FunctionComponent<FlyoutListProps> = ({
         throw new Error(`${response.status}`);
       }
       const data = await response.json();
-      setItems(data);
-      setFilteredItems(data);
+      setOriginalChatbots(data);
+      setFilteredChatbots(data);
       setIsLoading(false);
       setReloadList(false);
       setChatbots(data);
@@ -89,8 +106,8 @@ export const FlyoutList: React.FunctionComponent<FlyoutListProps> = ({
       return;
     }
     if (chatbots.length > 0) {
-      setItems(chatbots);
-      setFilteredItems(chatbots);
+      setOriginalChatbots(chatbots);
+      setFilteredChatbots(chatbots);
       setIsLoading(false);
       return;
     }
@@ -98,13 +115,18 @@ export const FlyoutList: React.FunctionComponent<FlyoutListProps> = ({
   };
 
   React.useEffect(() => {
-    loadAssistants();
+    if (typeWordPlural === 'assistants') {
+      loadAssistants();
+    }
+    if (typeWordPlural === 'files') {
+      // this is where you'd put your API call for files
+    }
   }, []);
 
   const buildMenu = () => {
     return (
       <MenuList>
-        {filteredItems.map((item) => (
+        {filteredChatbots.map((item) => (
           <MenuItem
             className="pf-chatbot__menu-item"
             itemId={item.name}
@@ -120,8 +142,8 @@ export const FlyoutList: React.FunctionComponent<FlyoutListProps> = ({
   };
 
   const onSelect = (_event: React.MouseEvent<Element, MouseEvent> | undefined, value) => {
-    if (filteredItems.length > 0) {
-      const newChatbot = items.filter((item) => item.name === value)[0];
+    if (filteredChatbots.length > 0) {
+      const newChatbot = originalChatbots.filter((item) => item.name === value)[0];
       updateFlyoutMenuSelectedChatbot(newChatbot);
       if (location.pathname !== '/') {
         navigate('/');
@@ -129,8 +151,8 @@ export const FlyoutList: React.FunctionComponent<FlyoutListProps> = ({
     }
   };
 
-  const findMatchingItems = (targetValue: string) => {
-    const matchingElements = items.filter((item) => {
+  const findMatchingItems = (targetValue: string, originalItems) => {
+    const matchingElements = originalItems.filter((item) => {
       const name = item.displayName ?? item.name;
       return name.toLowerCase().includes(targetValue.toLowerCase());
     });
@@ -138,21 +160,30 @@ export const FlyoutList: React.FunctionComponent<FlyoutListProps> = ({
   };
 
   const handleTextInputChange = (value: string) => {
-    if (value === '') {
-      setFilteredItems(items);
-      return;
+    if (typeWordPlural === 'assistants') {
+      if (value === '') {
+        setFilteredChatbots(originalChatbots);
+        return;
+      }
+      const newItems = findMatchingItems(value, originalChatbots);
+      setFilteredChatbots(newItems);
     }
-    const newItems = findMatchingItems(value);
-    setFilteredItems(newItems);
+    if (typeWordPlural === 'files') {
+      // you'll need to add a matcher here based on state
+    }
   };
 
-  const onClick = () => {
+  const onAssistantClick = () => {
     setError(undefined);
     loadAssistants();
   };
 
   return error ? (
-    <FlyoutError title={error.title} subtitle={error.body} onClick={onClick} />
+    <FlyoutError
+      title={error.title}
+      subtitle={error.body}
+      onClick={typeWordPlural === 'assistants' ? onAssistantClick : onRetry}
+    />
   ) : (
     <>
       <FlyoutHeader title={header} hideFlyout={hideFlyout} />
@@ -166,7 +197,7 @@ export const FlyoutList: React.FunctionComponent<FlyoutListProps> = ({
           />
           <Menu className="flyout-list-menu" isPlain onSelect={onSelect}>
             <MenuContent>
-              {filteredItems.length > 0 ? (
+              {filteredChatbots.length > 0 ? (
                 buildMenu()
               ) : (
                 <MenuList>
--- src/app/SidebarWithFlyout/SidebarWithFlyout.tsx
+++ src/app/SidebarWithFlyout/SidebarWithFlyout.tsx
@@ -8,12 +8,73 @@ import { FlyoutWizardProvider } from '@app/FlyoutWizard/FlyoutWizardContext';
 import { FlyoutList } from '@app/FlyoutList/FlyoutList';
 import { FlyoutWizard } from '@app/FlyoutWizard/FlyoutWizard';
 import { FlyoutForm } from '@app/FlyoutForm/FlyoutForm';
+import { useDropzone } from 'react-dropzone';
+import { getId } from '@app/utils/utils';
+import { ErrorObject } from '@app/types/ErrorObject';
+import { UserFacingFile } from '@app/types/UserFacingFile';
 
 export const SidebarWithFlyout: React.FunctionComponent = () => {
   const [sidebarHeight, setSidebarHeight] = useState(0);
   const [visibleFlyout, setVisibleFlyout] = useState(null);
+  const [isLoadingFile, setIsLoadingFile] = useState(false);
+  const [files, setFiles] = useState<UserFacingFile[]>([]);
+  const [error, setError] = useState<ErrorObject>();
   const sidebarRef = useRef<HTMLDivElement>(null);
   const flyoutMenuRef = useRef<HTMLDivElement>(null);
+
+  // example of how you can read a text file
+  const readFile = (file: File) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = () => reject(reader.error);
+      reader.readAsDataURL(file);
+    });
+
+  const { open, getInputProps } = useDropzone({
+    onDropAccepted: (files: File[]) => {
+      // handle file drop/selection
+      setIsLoadingFile(true);
+      // any custom validation you may want
+      if (files.length > 2) {
+        setFiles([]);
+        setError({ title: 'Uploaded more than two files', body: 'Upload fewer files' });
+        return;
+      }
+      // this is 200MB in bytes; size is in bytes
+      const anyFileTooBig = files.every((file) => file.size > 200000000);
+      if (anyFileTooBig) {
+        setFiles([]);
+        setError({ title: 'Uploaded a file larger than 200MB.', body: 'Try a uploading a smaller file' });
+        return;
+      }
+
+      const newFiles = files.map((file) => {
+        return {
+          name: file.name,
+          id: getId(),
+        };
+      });
+      setFiles(newFiles);
+
+      // example of how to read file - this is where you'd send it to the server and trigger some sort of loading state
+      files.forEach((file) => {
+        readFile(file)
+          .then((data) => {
+            // eslint-disable-next-line no-console
+            console.log(data);
+            setError(undefined);
+            // this is just for demo purposes, to make the loading state really obvious
+            setTimeout(() => {
+              setIsLoadingFile(false);
+            }, 1000);
+          })
+          .catch((error: DOMException) => {
+            setError({ title: 'Failed to read file', body: error.message });
+          });
+      });
+    },
+  });
 
   // Capture sidebar height initially and whenever it changes.
   // We use this to control the flyout height.
@@ -84,6 +145,33 @@ export const SidebarWithFlyout: React.FunctionComponent = () => {
         </FlyoutWizardProvider>
       );
     }
+    if (visibleFlyout === 'Files') {
+      return (
+        <FlyoutWizardProvider>
+          {/* this is required for upload function open() to work in Safari and Firefox */}
+          <input {...getInputProps()} />
+          <FlyoutWizard
+            steps={[
+              <FlyoutList
+                key="files-list"
+                hideFlyout={() => setVisibleFlyout(null)}
+                buttonText="New file"
+                typeWordPlural="files"
+                title={visibleFlyout}
+                onFooterButtonClick={() => {
+                  open();
+                }}
+                error={error}
+                isLoading={isLoadingFile}
+                onRetry={() => {
+                  open();
+                }}
+              />,
+            ]}
+          />
+        </FlyoutWizardProvider>
+      );
+    }
     return;
   };
 
@@ -111,6 +199,14 @@ export const SidebarWithFlyout: React.FunctionComponent = () => {
               aria-expanded={visibleFlyout === 'Assistants'}
             >
               Assistants
+            </NavItem>
+            <NavItem
+              component="button"
+              onClick={toggleFlyout}
+              aria-haspopup="dialog"
+              aria-expanded={visibleFlyout === 'Files'}
+            >
+              Files
             </NavItem>
           </NavList>
         </Nav>
